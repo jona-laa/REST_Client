7b371af8d46e7b33d20c12396e0a44bab6a9c06b
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2020-09-28 15:47:35 +0200

    add -form and course icon support

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,36 @@
 // DOM Elements
 const coursesContainer = document.querySelector('#courses-container')
+const creditsTotal = document.querySelector('#credits-total')
 
 const getCourses = () => {
   fetch('http://localhost:8080/DEMO_REST/api/courses')
     .then(res => res.json())
     .then(data => data.courses.forEach(course => createElement(course)))
+    .catch(e => console.log(e))
 }
 
+let credits = 0;
+
 const createElement = (course) => {
-  console.log(course)
   coursesContainer.innerHTML += `
-    <div>
+    <div class="courses-container_course">
+    <i class="${course.icon} fa-4x"></i>
       <a href="${course.link}" class="icon-link react" target="_blank">
-        <i class="fas fa-university fa-3x"></i>
         <h3>${course.name}</h3>
       </a>
       <p>${course.code}</p>
       <p>${course.progression}</p>
+      <p>${course.credits}</p>
     </div>
-  `
+  `;
+  credits += parseFloat(course.credits)
+  creditsTotal.innerHTML = credits;
+}
+
+const addCourse = (e) => {
+  e.preventDefault();
+  fetch()
+  console.log('added')
 }
 
 window.addEventListener("load", getCourses);
-
-/*
-Lägg till HP per kurs samt HP totalt
-
-WU I - HTML5
-
-
-*/
